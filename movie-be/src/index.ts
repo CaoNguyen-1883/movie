627b841e0f73623df949eeb0b0222df5f20bd655
CaoNguyen-1883 <nguyendadenkrb@gmail.com>
2025-05-16 14:01:27 +0700

    init

---
+++ movie-be/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Hello World");
+});
+
+
+app.listen(port, ( ) => {
+    console.log(`Server is running on port ${port}`);
+});
